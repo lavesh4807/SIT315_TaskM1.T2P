2fcf5fdbd38b0b497b3a6fa7975feb8d3e901176
lavesh4807 <142747980+lavesh4807@users.noreply.github.com>
2024-02-01 16:31:51 +0530

    MT1P.ino

--- MT1P.ino
+++ MT1P.ino
@@ -1,32 +1,60 @@
-#define pirPin 5
-#define ledPin 6
+class MotionDetector {
+public:
+  virtual void handleMotion() = 0;
+  virtual void handleNoMotion() = 0;
+};
 
-volatile bool ismotionDetected = false;
+class SimpleMotionDetector : public MotionDetector {
+private:
+  int statusIndicatorPin;
+  volatile int* detectionStatus;
 
-void interruptPIR() {
-    ismotionDetected = true;
-}
+public:
+  SimpleMotionDetector(int indicatorPin, volatile int* status)
+      : statusIndicatorPin(indicatorPin), detectionStatus(status) {}
+
+  void handleMotion() override {
+    digitalWrite(statusIndicatorPin, HIGH);
+    Serial.println("Motion detected!");
+    delay(1000);  // Adjust this delay cautiously; it may affect interrupt responsiveness
+    digitalWrite(statusIndicatorPin, LOW);
+    *detectionStatus = 1;
+  }
+
+  void handleNoMotion() override {
+    Serial.println("No motion detected");
+    // Consider whether you want to reset detectionStatus to 0 here
+    // Depending on your application, you may want to keep the last status until a new motion event occurs.
+    // *detectionStatus = 0;
+  }
+};
+
+const int motionSensorInterrupt = 2;
+const int statusIndicatorPin = 6;
+volatile int detectionStatus = 0;
+
+MotionDetector* motionDetector;
 
 void setup() {
-    pinMode(pirPin, INPUT);
-    pinMode(ledPin, OUTPUT);
-    Serial.begin(9600);
+  Serial.begin(9600);
+  Serial.println("Motion Detection System Initialized");
 
-    attachInterrupt(digitalPinToInterrupt(pirPin), interruptPIR, RISING);
+  pinMode(statusIndicatorPin, OUTPUT);
+  motionDetector = new SimpleMotionDetector(statusIndicatorPin, &detectionStatus);
+  attachInterrupt(digitalPinToInterrupt(motionSensorInterrupt), handleMotionInterrupt, HIGH);
 }
 
 void loop() {
-
-    if(ismotionDetected){
-        digitalWrite(ledPin, HIGH);
-        Serial.println("Motion detected! Turning on LED.");
-        ismotionDetected = false;
-    }
-    else{
-        digitalWrite(ledPin, LOW);
-        Serial.println("No motion detected. Turning off LED.");
-    }
-   
-    delay(1000);
+  // The loop is intentionally left empty
+  // All logic is moved to the handleMotionInterrupt function
 }
 
+void handleMotionInterrupt() {
+  int sensorValue = digitalRead(motionSensorInterrupt);
+
+  if (sensorValue == HIGH) {
+    motionDetector->handleMotion();
+  } else {
+    motionDetector->handleNoMotion();
+  }
+}
