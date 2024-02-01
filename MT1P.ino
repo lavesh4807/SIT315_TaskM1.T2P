da8546feed39cb207ce851bed7bedfb381975c18
lavesh4807 <142747980+lavesh4807@users.noreply.github.com>
2024-02-01 15:20:19 +0530

    MT1P.ino

---
+++ MT1P.ino
@@ -0,0 +1,32 @@
+#define pirPin 5
+#define ledPin 6
+
+volatile bool ismotionDetected = false;
+
+void interruptPIR() {
+    ismotionDetected = true;
+}
+
+void setup() {
+    pinMode(pirPin, INPUT);
+    pinMode(ledPin, OUTPUT);
+    Serial.begin(9600);
+
+    attachInterrupt(digitalPinToInterrupt(pirPin), interruptPIR, RISING);
+}
+
+void loop() {
+
+    if(ismotionDetected){
+        digitalWrite(ledPin, HIGH);
+        Serial.println("Motion detected! Turning on LED.");
+        ismotionDetected = false;
+    }
+    else{
+        digitalWrite(ledPin, LOW);
+        Serial.println("No motion detected. Turning off LED.");
+    }
+   
+    delay(1000);
+}
+
